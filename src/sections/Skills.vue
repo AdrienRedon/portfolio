d7f241ce4a2d3b6345375f61256dda69b6ddeda2
AdrienRedon <redon.adrien@gmail.com>
2020-06-11 15:23:42 +0200

    Update skills\

--- src/sections/Skills.vue
+++ src/sections/Skills.vue
@@ -3,48 +3,32 @@
         <h2>Mes compétences</h2>
         <div class="skills">
             <div class="skill">
-                <h3>Dévelopement Web</h3>
+                <h3>Dévelopement Front End</h3>
                 <ul>
-                    <li>{{ php }} ans d'expérience en PHP (Laravel, Symfony)</li>
-                    <li>{{ ruby }} ans avec Ruby (Ruby on Rails)</li>
-                    <li>{{ node }} ans avec NodeJS (Express, Sails, Restify, Hapi)</li>
+                    <li>JavaScript/TypeScript</li>
+                    <li>Angular/Vue.js/React.js</li>
+                    <li>Jest/Cypress</li>
                 </ul>
             </div>
             <div class="skill">
-                <h3>Dévelopement Mobile</h3>
+                <h3>Dévelopement Back End</h3>
                 <ul>
-                    <li>Développement d'applications hybrides à l'aide d'Angular 2 et Ionic 2</li>
-                    <li>Plusieurs projets cross-plateforme développés avec React Native</li>
+                    <li>Node.js (Express)</li>
+                    <li>PHP (Symfony, Laravel)</li>
+                    <li>SQL (Oracle, PostgreSQL, MySQL)</li>
                 </ul>
             </div>
             <div class="skill">
                 <h3>Autres</h3>
                 <ul>
-                    <li>Versioning et intégration continue avec git/svn et Jenkins</li>
-                    <li>Utilisation d'outil de design comme Sketch ou Photoshop</li>
+                    <li>Versioning et intégration continue avec git/svn et Jenkins/CircleCI</li>
+                    <li>Gestion de projets, méthodes agiles, SCRUM, management</li>
+                    <li>Utilisation d'outil de design comme Sketch, Figma ou Photoshop</li>
                 </ul>
             </div>
         </div>
     </div>
 </template>
-
-<script>
-    import moment from 'moment';
-
-    export default {
-        computed: {
-            php () {
-                return moment().diff('2011-07-15', 'years');
-            },
-            ruby () {
-                return moment().diff('2014-03-30', 'years');
-            },
-            node () {
-                return moment().diff('2015-10-20', 'years');
-            },
-        }
-    }
-</script>
 
 <style scoped>
     #skills {
