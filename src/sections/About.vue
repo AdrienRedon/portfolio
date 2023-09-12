906aa3e61ffde577cc782fc91e1e4f74161706bb
Adrien REDON <AdrienRedon@users.noreply.github.com>
2023-09-12 14:23:59 +0200

    Update about and header

--- src/sections/About.vue
+++ src/sections/About.vue
@@ -1,116 +1,131 @@
 <template>
-    <portfolio-section class="wrapper">
-        <h2>Qui suis-je ?</h2>
-        <div class="container">
-            <div class="flex">
-                <h3>Adrien Redon, {{ age }} ans</h3>
-                <h3>Développeur Web</h3>
-                <p>Bonjour, je suis originaire de la région lyonnaise et diplomé Ingénieur en informatique à l'école CPE Lyon. Après une expérience de 3 ans en tant que développeur web au sein de la DSI d'Orange, je suis aujourd'hui Lead tech front à Geolid.</p>
-                <p>Passionné par les nouvelles technologies, je prends plaisir à me tenir à jour dans ma veille. Smart Home, VR / AR, AI sont des sujets sur lesquels je garde un oeil.</p>
-                <p>J'aime aussi voyager et je pratique de nombreux sports (escalade, triathlon, snowboard, yoga, etc...)</p>
-                <p>Ce portfolio a pour but de présenter certaines de mes réalisations. Vous pouvez également visualiser mon CV et le télécharger.</p>
-            </div>
-            <div>
-                <div class="picture"> 
-                    <img src="/assets/img/snowboard.png">
-                </div>
-            </div>
+  <portfolio-section class="wrapper">
+    <h2>Qui suis-je ?</h2>
+    <div class="container">
+      <div class="flex">
+        <h3>Adrien Redon, {{ age }} ans</h3>
+        <h3>Développeur Web</h3>
+        <p>
+          Bonjour, je suis originaire de la région lyonnaise et diplomé
+          Ingénieur en informatique à l'école CPE Lyon. Après plusieurs
+          expériences en tant que développeur web et Lead développeur, je suis
+          aujourd'hui Lead développeur à Geolid.
+        </p>
+        <p>
+          Passionné par les nouvelles technologies, je prends plaisir à me tenir
+          à jour dans ma veille. Smart Home, VR / AR, AI sont des sujets sur
+          lesquels je garde un oeil.
+        </p>
+        <p>
+          J'aime aussi voyager et je pratique de nombreux sports (escalade,
+          triathlon, snowboard, yoga, etc...)
+        </p>
+        <p>
+          Ce portfolio a pour but de présenter certaines de mes réalisations.
+          Vous pouvez également visualiser mon CV et le télécharger.
+        </p>
+      </div>
+      <div>
+        <div class="picture">
+          <img src="/assets/img/snowboard.png" />
         </div>
-        <a href="/assets/AdrienREDON.pdf" class="btn">Télécharger</a>
-        <skills id="skills"></skills>
-    </portfolio-section>
+      </div>
+    </div>
+    <a href="/assets/AdrienREDON.pdf" class="btn">Télécharger</a>
+    <skills id="skills"></skills>
+  </portfolio-section>
 </template>
 
 <script>
-    import moment from 'moment';
-    import PortfolioSection from '../components/PortfolioSection.vue';
-    import Skills from './Skills.vue';
+import moment from "moment";
+import PortfolioSection from "../components/PortfolioSection.vue";
+import Skills from "./Skills.vue";
 
-    export default {
-        components: {
-            PortfolioSection,
-            Skills,
-        },
-        computed: {
-            age () {
-                return moment().diff('1996-03-21', 'years');
-            }
-        }
-    }
+export default {
+  components: {
+    PortfolioSection,
+    Skills,
+  },
+  computed: {
+    age() {
+      return moment().diff("1996-03-21", "years");
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .wrapper {
-        display: -webkit-flex;
-        display: -moz-flex;
-        display: -ms-flex;
-        display: -o-flex;
-        display: flex;
-        flex-direction: column;
-    }
-    .container {
-        display: -webkit-flex;
-        display: -moz-flex;
-        display: -ms-flex;
-        display: -o-flex;
-        display: flex;
-        padding: 0 20px;
-        text-align: justify;
-        flex-wrap: wrap;
-        justify-content: space-around;
-        align-items: center;
-    }
+.wrapper {
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+  display: flex;
+  flex-direction: column;
+}
+.container {
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+  display: flex;
+  padding: 0 20px;
+  text-align: justify;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+}
 
-    @media screen and (min-width: 700px) {
-        .container > div {
-            max-width: 60%;
-        }
-    }
+@media screen and (min-width: 700px) {
+  .container > div {
+    max-width: 60%;
+  }
+}
 
-    h3 {
-    text-align: left;
-    } 
+h3 {
+  text-align: left;
+}
 
-    h3 {
-        margin: 0;
-    }
+h3 {
+  margin: 0;
+}
 
-    .picture {
-        height: 300px;
-        width: 300px;
-        border-radius: 50%;
-        overflow: hidden;
-    }
+.picture {
+  height: 300px;
+  width: 300px;
+  border-radius: 50%;
+  overflow: hidden;
+}
 
-    .picture img {
-        height: 300px;
-        width: 300px;
-    }
+.picture img {
+  height: 300px;
+  width: 300px;
+}
 
-    #skills {
-        flex: 1;
-        margin-top: 50px;
-        padding-bottom: 20px;
-    }
-    .btn {
-        display: inline-block;
-        margin: auto;
-        height: 36px;
-        line-height: 36px;
-        vertical-align: middle;
-        padding: 0 2rem;
+#skills {
+  flex: 1;
+  margin-top: 50px;
+  padding-bottom: 20px;
+}
+.btn {
+  display: inline-block;
+  margin: auto;
+  height: 36px;
+  line-height: 36px;
+  vertical-align: middle;
+  padding: 0 2rem;
 
-        text-align: center;
-        text-decoration: none;
-        color: #fff;
-        letter-spacing: .5px;
-        
-        background-color: #F05F40;
-        border: none;
-        border-radius: 2px;
-        
-        text-transform: uppercase;
-        transition: .2s ease-out;
-        cursor: pointer;
-    }
+  text-align: center;
+  text-decoration: none;
+  color: #fff;
+  letter-spacing: 0.5px;
+
+  background-color: #f05f40;
+  border: none;
+  border-radius: 2px;
+
+  text-transform: uppercase;
+  transition: 0.2s ease-out;
+  cursor: pointer;
+}
 </style>
--- src/sections/PortfolioHeader.vue
+++ src/sections/PortfolioHeader.vue
@@ -1,65 +1,65 @@
 <template>
-    <header>
-        <h1>
-            Adrien REDON – {{ age }} ans<br>
-            Développeur Web depuis {{ experience }} ans<br>
-            Actuellement Lead Développeur Angular à Notilo Plus
-        </h1>
-        <scroll-button href="#about" class="button"></scroll-button>
-    </header>
+  <header>
+    <h1>
+      Adrien REDON – {{ age }} ans<br />
+      Développeur Web depuis {{ experience }} ans<br />
+      Actuellement Lead Développeur Angular à Geolid
+    </h1>
+    <scroll-button href="#about" class="button"></scroll-button>
+  </header>
 </template>
 
 <script>
-    import moment from 'moment';
-    import ScrollButton from '../components/ScrollButton.vue';
+import moment from "moment";
+import ScrollButton from "../components/ScrollButton.vue";
 
-    export default {
-        components: {
-            ScrollButton,
-        },
-        computed: {
-            age () {
-                return moment().diff('1996-03-21', 'years');
-            }, 
-            experience () {
-                return moment().diff('2011-07-15', 'years');
-            },
-        }
-    }
+export default {
+  components: {
+    ScrollButton,
+  },
+  computed: {
+    age() {
+      return moment().diff("1996-03-21", "years");
+    },
+    experience() {
+      return moment().diff("2011-07-15", "years");
+    },
+  },
+};
 </script>
 
 <style scoped>
-    header {
-        display: -webkit-flex;
-        display: -moz-flex;
-        display: -ms-flex;
-        display: -o-flex;
-        display: flex;
-        -webkit-flex-direction: column;
-        -moz-flex-direction: column;
-        -ms-flex-direction: column;
-        -o-flex-direction: column;
-        flex-direction: column;
-        background: url(/assets/img/header.jpg) center no-repeat;
-        background-size: cover;
-        height: 100vh;
-    }
+header {
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+  display: flex;
+  -webkit-flex-direction: column;
+  -moz-flex-direction: column;
+  -ms-flex-direction: column;
+  -o-flex-direction: column;
+  flex-direction: column;
+  background: url(/assets/img/header.jpg) center no-repeat;
+  background-size: cover;
+  height: 100vh;
+}
 
-    h1 {
-        text-transform: uppercase;
-        margin: auto;
-        text-align: center;
-        color: white;
-        font-size: 24px;
-    }
+h1 {
+  text-transform: uppercase;
+  margin: auto;
+  text-align: center;
+  color: white;
+  font-size: 24px;
+}
 
-    @media screen and (min-width: 700px) {
-        h1 {
-            font-size: 32px;
-        }
-    }
+@media screen and (min-width: 700px) {
+  h1 {
+    font-size: 32px;
+  }
+}
 
-    .button {
-        margin: 0 auto;
-    }
+.button {
+  margin: 0 auto;
+}
 </style>
